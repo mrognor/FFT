1e120141c2c2c01cc121dd11bd57d88acd495f61
Timur Ammaev <timurlego1@yandex.ru>
2023-04-15 18:13:26 +0300

    Optimization

--- fft.cpp
+++ fft.cpp
@@ -86,32 +86,71 @@ std::vector<complex> FFT(const std::vector<T>& x)
         }
     }
 
+    int64_t halfElem = arrLen / 8;
     std::vector<complex> tmp(arrLen);
     std::vector<complex> w(arrLen / 2);
+
+    // Pre calculate constans W in complex circle
+    // Calculate 0 value
+    w[0].Re = 1;
+    w[0].Im = 0;
+
+    // Calculate middle value
+    w[arrLen / 4].Re = cos(2 * PI * (arrLen / 4) / arrLen);
+    w[arrLen / 4].Im = -sin(2 * PI * (arrLen / 4) / arrLen);
+
+    // First and fourth quad
+    //         |   x = 1
+    //         |  
+    //    -----------
+    //         |
+    //         |   x = 4
+    for (int64_t i = 1; i <= halfElem; ++i)
+    {
+        // 1 quad
+        w[i].Re = cos(2 * PI * i / arrLen);
+        w[i].Im = -sin(2 * PI * i / arrLen);
+
+        // 4 quad
+        w[w.size() - i].Re = -w[i].Re;
+        w[w.size() - i].Im = w[i].Im;
+    }
+
+    // Second and third quad
+    // 2 = x   |   
+    //         |  
+    //    -----------
+    //         |
+    // 3 = x   |   
+    for (int64_t i = 1; i < halfElem; ++i)
+    {
+        // 2 quad
+        w[halfElem + i].Re = -w[(halfElem - i) % halfElem].Im;
+        w[halfElem + i].Im = -w[(halfElem - i) % halfElem].Re;
+
+        // 3 quad
+        w[w.size() - 2 * halfElem + i].Re = w[i].Im;
+        w[w.size() - 2 * halfElem + i].Im = -w[i].Re;
+    }
 
     int64_t elemCount = 2;
     // Recursion cycle
     for (int64_t i = 0; i < recursiveLen; ++i)
     {
         // Calculate array with w
-        int64_t halfElem = elemCount / 2;
-        for (int64_t j = 0; j < halfElem; ++j)
-        {
-            w[j].Re = cos(2 * PI * j / elemCount);
-            w[j].Im = -sin(2 * PI * j / elemCount);
-        }
+        halfElem = elemCount / 2;
 
         // Iterate over all array by chunks with elemCount
         for (int64_t j = 0; j < arrLen; j += elemCount)
         {
-            for (int k = 0; k < halfElem; ++k)
+            for (int64_t k = 0; k < halfElem; ++k)
             {
-                tmp[j + k] = res[j + k] + w[k] * res[j + k + halfElem];
-                tmp[j + halfElem + k] = res[j + k] - w[k] * res[j + k + halfElem];
+                tmp[j + k] = res[j + k] + w[k * (arrLen / elemCount)] * res[j + k + halfElem];
+                tmp[j + halfElem + k] = res[j + k] - w[k * (arrLen / elemCount)] * res[j + k + halfElem];
             }
         }
 
-        res = tmp; // Optimize
+        res = tmp;
         elemCount <<= 1;
     }
 
@@ -144,7 +183,7 @@ std::vector<int64_t> IFFT(const std::vector<complex>& X, const uint64_t& dataLen
         {
             if (count < preRes.size() - 1) res.push_back(round(it->abs()) / preRes.size());
             ++count;
-        } 
+        }
     }
     return res;
 }
@@ -155,7 +194,7 @@ int main()
     std::vector<complex> fft;
     std::vector<int64_t> ifft;
 
-    for (int64_t i = 0; i < 39000; ++i)
+    for (int64_t i = 0; i < 65536; ++i)
         input.push_back(rand());
 
     auto start(std::chrono::high_resolution_clock::now());
@@ -165,5 +204,6 @@ int main()
     auto end(std::chrono::high_resolution_clock::now());
     auto duration(std::chrono::duration_cast<std::chrono::nanoseconds>(end - start));
 
-    std::cout << "Time to operations: " << (long double)duration.count() / 1000000 << std::endl;
+    std::cout << "Time to operations: " << duration.count() << std::endl;
+    std::cout << "Is all ok: " << (IFFT(fft, input.size()) == input) << std::endl;
 }
