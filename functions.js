2e954a35decb230da549e82329706f83c266f6b0
Ammaev Timur <timurlego1@yandex.ru>
2024-05-02 13:22:16 +0300

    Added almost Fourier transform

--- functions.js
+++ functions.js
@@ -3,26 +3,17 @@ function plot() {
     const sinArg = document.getElementById('range').value;
     const polarRatio = document.getElementById('polar-range').value;
 
-    // Const to set sinus length
-    const sinLen = 6;
-
-    // Graphics axis names
-    var layout = { 
-        xaxis: { title: 'X-axis', range: [0, sinLen * Math.PI] }, 
-        yaxis: { title: 'Y-axis', range: [-1, 1]}
-    }; 
-
     // Plot base sin
     const graph = document.getElementById('graph'); 
     graph.innerHTML = ''; 
 
     var xValues = [];
-    for (let i = 0; i <= sinLen * Math.PI; i += 0.01) {
+    for (let i = 0; i <= 6 * Math.PI; i += 0.01) {
         xValues.push(i);
     }
 
     var yValues = [];
-    for (let i = 0; i <= sinLen * Math.PI; i += 0.01) {
+    for (let i = 0; i <= 6 * Math.PI; i += 0.01) {
         yValues.push(Math.sin(sinArg * i));
     } 
 
@@ -31,6 +22,11 @@ function plot() {
         y: yValues, 
         type: 'scatter', 
         mode: 'lines', 
+    }; 
+
+    var layout = { 
+        xaxis: { title: 'X-axis', range: [-0.5, 6 * Math.PI + 0.5] }, 
+        yaxis: { title: 'Y-axis'},
     }; 
 
     Plotly.newPlot(graph, [trace], layout); 
@@ -57,15 +53,101 @@ function plot() {
         yaxis: { title: 'Y-axis', range: [-1.1, 1.1]}
     }; 
 
-    trace = { 
+    var massX = 0;
+    var massY = 0;
+    for (let i = 0; i < xPolarValues.length; i++) {
+        massX += xPolarValues[i];
+        massY += yPolarValues[i];
+    }
+
+    var graphTrace = { 
         x: xPolarValues, 
         y: yPolarValues, 
         type: 'scatter', 
         mode: 'lines', 
     }; 
 
-    Plotly.newPlot(polarGraph, [trace], layout); 
+    massX /= xPolarValues.length;
+    massY /= xPolarValues.length;
+
+    var massTrace = {
+        x: [massX], 
+        y: [massY], 
+        type: 'scatter', 
+        mode: 'marker',
+        marker: {
+            size: 10,
+        }
+    };
+
+    Plotly.newPlot(polarGraph, [graphTrace, massTrace], layout); 
 
     document.getElementsByClassName("main-svg")[3].style.cssText = "background: rgba(0, 0, 0, 0);";
     document.getElementById("polar-label").textContent = "Частота намотки: " + polarRatio;
+
+
+    // Mass graphic
+    var xValues = [];
+    for (let i = 0; i <= 6 * Math.PI; i += 0.01) {
+        xValues.push(i);
+    }
+
+    var yValues = [];
+    for (let i = 0; i <= 6 * Math.PI; i += 0.01) {
+        yValues.push(Math.sin(sinArg * i));
+    }
+
+    var x = [];
+    var y = [];
+
+    for (let i = 1; i <= 6; i += 0.01) {
+        var xPolarValues = [];
+        var yPolarValues = [];
+
+        for (let k = 0; k < xValues.length; k++) {
+            xPolarValues.push(yValues[k] * Math.sin(xValues[k] * i));
+            yPolarValues.push(yValues[k] * Math.cos(xValues[k] * i));
+        }
+
+        var mass = 0;
+        for (let i = 0; i < xPolarValues.length; i++) {
+            mass += xPolarValues[i];
+        }
+        mass /= xPolarValues.length;
+
+        x.push(i);
+        y.push(mass);
+    }
+
+    var layout = { 
+        xaxis: { title: 'X-axis', range: [0.8, 6.2] }, 
+        yaxis: { title: 'Y-axis'}
+    };
+
+    console.log(x);
+    console.log(y);
+
+    var graphTrace = {
+        x: x, 
+        y: y, 
+        type: 'scatter', 
+        mode: 'lines', 
+    }; 
+
+    var massTrace = {
+        x: [polarRatio], 
+        y: [massX], 
+        type: 'scatter', 
+        mode: 'marker',
+        marker: {
+            size: 10,
+        }
+    };
+
+    const massGraph = document.getElementById('mass-graph'); 
+    massGraph.innerHTML = ''; 
+
+    Plotly.newPlot(massGraph, [graphTrace, massTrace], layout); 
+
+    document.getElementsByClassName("main-svg")[6].style.cssText = "background: rgba(0, 0, 0, 0);";
 }
